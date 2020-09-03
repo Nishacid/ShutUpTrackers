3be3a31b1bc6d5be75c5ba112009c3d8b5e84c7c
0xx66 <0xx66@protonmail.com>
2020-09-03 23:32:59 +0200

    Adding Licence to source

--- myinfo.php
+++ myinfo.php
@@ -446,7 +446,7 @@ $https = (!empty($_SERVER['HTTPS'])) ? $_SERVER['HTTPS'] : 'OFF';
     <?php require('./inc/contact.php');?>
     <footer>
     Shutuptrackers is a website that provides information for protecting your data security and privacy. No Ads, No Analytics, No Affiliates, No Cross-Site Requests.
-    <small><a href="./thanks.php" class="thanks">Thanks page</a></small>
+    <small><a href="./thanks.php" class="thanks">Thanks page</a> / <a href="./LICENCE.txt" class="thanks">Licence</a></small>
     </footer>
     <script src="js/main.js"></script>
 
